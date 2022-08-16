69322f70b4f41f2354254314a1a22cda24c0f578
Snidha19 <tasfiaahsan7@gmail.com>
2022-08-16 10:49:18 +0600

    fetch API data and filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import {
+  Typography,
+  Card,
+  Container,
+  Box,
+  TextField,
+} from "@mui/material";
 
-function App() {
+const App = () => {
+  const [categories, setCategories] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    const getApiData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.publicapis.org/categories`
+        );
+        setCategories(response.data.categories);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getApiData();
+  });
+
+  const filteredCategories =
+    searchInput === ""
+      ? categories
+      : categories.filter((item) => item.toLowerCase().search(searchInput.toLowerCase()) > -1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Container sx={{ my: 3, mt: 4}}>
+        <Typography
+          variant="h4"
+          component="div"
+          gutterBottom
+          sx={{ fontWeight: "bold" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Filtering
+        </Typography>
+        <Box>
+          <TextField
+            label="Enter text"
+            variant="outlined"
+            onChange={(event) => setSearchInput(event.target.value)}
+            sx={{
+              height: 50,
+              borderRadius: 3,
+              minWidth: 200,
+              maxWidth: "auto",
+            }}
+          />
+        </Box>
+      </Container>
+      {filteredCategories.map((item, index) => {
+        return (
+          <React.Fragment key={index}>
+          <Container sx={{ my: 4 }}>
+            <Card sx={{ mb: 3, borderRadius: 2 }} elevation={5}>
+              <Box
+                sx={{
+                  width: "auto",
+                  alignItems: "center",
+                  margin: { xs: 1, md: 2 },
+                }}
+              >
+                <Typography
+                  sx={{
+                    mx: { xs: 1, md: 2 },
+                    fontSize: { xs: 12, md: 18 },
+                    fontWeight: "bold",
+                  }}
+                >
+                  {item}
+                </Typography>
+              </Box>
+            </Card>
+          </Container>
+          </React.Fragment>
+        );
+      })}
+    </>
   );
-}
+};
 
 export default App;
